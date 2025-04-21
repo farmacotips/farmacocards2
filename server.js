9b003822ae46b15ed6fc4c039ceef46007010a80
farmacotips <jjflores8@gmail.com>
2025-04-20 22:08:30 -0400

    Habilitar ruta para mazos publicos

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express'), multer = require('multer'), csv = require('c
 const app = express(), upload = multer({ dest: 'uploads/' });
 let flashcards = [];
 app.use(express.static('public'));
+const path = require('path'); // Si no lo tienes ya
+app.use(express.static('public'));
+app.get('/mazos-publicos', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'mazos-publicos.json'));
+});
+
 app.post('/upload', upload.single('csv'), (req, res) => {
   const results = [];
   fs.createReadStream(req.file.path)
